95b4766d1efe410bb982551fa7512f1e8f5d536b
unknown <ayt.ullah.dev@gmail.com>
2022-04-30 19:58:43 +0600

    add and Get product implimented

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const cors = require('cors');
 require('dotenv').config()
 const app = express();
+app.use(cors())
+app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.rph41.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
         await client.connect();
         const userCollection = client.db("dobybox").collection('products');
-        const product = {name:'Shoes',desc:'If a dog chews shoes whose shoes does he choose?',price:120,supplier:'ayt',quan:20,img:'https://api.lorem.space/image/shoes?w=400&h=225'};
-       //const result = await userCollection.insertOne(product);
-        console.log("product inserted");
+        
+        app.post('/addproduct',async(req,res)=>{
+            const tempdata=req.body;
+            const data = {name:tempdata.pname,price:tempdata.pprice,quan:tempdata.pquan,supplier:tempdata.psupplier,img:tempdata.pimg,desc:tempdata.pdesc}
+            const result = await userCollection.insertOne(data);
+            res.send(result);
+        })
+        app.get('/products',async(req,res)=>{
+            const querry={};
+            const cursor = userCollection.find(querry);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
 
     }finally{
 
